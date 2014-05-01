4c503c31835d593ff31633d0d73cb14d190da594
unknown <rakuth@gmail.com>
2014-05-01 14:44:46 -0700

    fixing forces

--- LiftNormalizeDirection.m
+++ LiftNormalizeDirection.m
@@ -1,13 +1,13 @@
 %Correction function for normaization of lift direction.
 %Last matrix with negative signs accounts for opposing of direction of rotation
 
-function output = LiftNormalizeDirection(V_x_inertial, V_y_inertial)
-
-if((V_x_inertial^2 + V_y_inertial^2) == 0)
-    output = [0;0;0];
-else
-    output = [-V_x_inertial/sqrt(V_x_inertial^2 + V_y_inertial^2);...
-              -V_y_inertial/sqrt(V_x_inertial^2 + V_y_inertial^2);...
-              0];
-end
+function output = LiftNormalizeDirection(V_x_inertial, V_y_inertial, V_z_inertial, wind)
+    airspeed2 = MagVelocityWithWindSquared(V_x_inertial, V_y_inertial, V_z_inertial, wind);
+    if((sqrt(airspeed2) == 0)
+        output = [0;0;0];
+    else
+        orientationVector = [ ; ; ];
+        windVector = [ ; ; ];
+        output = ;
+    end
 end
